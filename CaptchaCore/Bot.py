189767857740bb95f3732cfab5ffa79ec3c30285
sudoskys <coldlando@hotmail.com>
2022-08-29 16:19:37 +0800

    修复Bug

--- CaptchaCore/Bot.py
+++ CaptchaCore/Bot.py
@@ -53,9 +53,9 @@ class clinetBot(object):
         if _csonfig.get("statu"):
             Tool().console.print("Bot Running", style='blue')
             bot, config = self.botCreate()
-            from telebot import asyncio_helper
-            asyncio_helper.proxy = 'http://127.0.0.1:7890'  # url
-            print("正在使用代理！")
+            # from telebot import asyncio_helper
+            # asyncio_helper.proxy = 'http://127.0.0.1:7890'  # url
+            # print("正在使用代理！")
             # from telebot import custom_filters
 
             from telebot import types, util
