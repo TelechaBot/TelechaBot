1ff24662249807be926696767bacc98bb5a62208
sudoskys <coldlando@hotmail.com>
2022-08-29 17:19:28 +0800

    修复Bug

--- CaptchaCore/Bot.py
+++ CaptchaCore/Bot.py
@@ -56,7 +56,7 @@ class clinetBot(object):
             # from telebot import asyncio_helper
             # asyncio_helper.proxy = 'http://127.0.0.1:7890'  # url
             # print("正在使用代理！")
-            # from telebot import custom_filters
+
 
             from telebot import types, util
             import CaptchaCore.BotEvent
