9ff1c206bb3c1439870850098da29807b4411471
sudoskys <coldlando@hotmail.com>
2022-10-03 15:44:17 +0800

    feat(project):优化队列库

--- Bot/Redis.py
+++ Bot/Redis.py
@@ -1,232 +1,184 @@
 # -*- coding: utf-8 -*-
-# @Time    : 8/22/22 9:28 PM
-# @FileName: Redis.py
+# @Time    : 9/29/22 10:04 PM
+# @FileName: __init__.py.py
 # @Software: PyCharm
 # @Github    ：sudoskys
-import time
-import ast
-import redis  # 导入redis 模块
 import datetime
+import json
+import time
+import redis
+import uuid
+import multiprocessing
 
-pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+task_lock = multiprocessing.Lock()
+_redis_pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+_redis = redis.Redis(host='localhost', port=6379, decode_responses=True)
+_MsgTask = {}
 
-
-# 必须需要一个创建机器人对象的类才能使用KickMember功能！
 
 class JsonRedis(object):
-    def __init__(self, interval=175):
-        JsonRedis.load_tasks()
-        if not _tasks.get("interval"):
-            if interval:
-                _tasks["interval"] = interval
-                JsonRedis.save_tasks()
-        if not _tasks.get("Time_id"):
-            _tasks["Time_id"] = {}
-            JsonRedis.save_tasks()
-        if not _tasks.get("Time_group"):
-            _tasks["Time_group"] = {}
-            JsonRedis.save_tasks()
-        if not _tasks.get("User_group"):
-            _tasks["User_group"] = {}
-            JsonRedis.save_tasks()
-        if not _tasks.get("super"):
-            _tasks["super"] = {}
-            JsonRedis.save_tasks()
 
     @staticmethod
-    def load_tasks():
-        global _tasks
-        task = r.get('tasks')
-        if task is not None:
-            _tasks = ast.literal_eval(task)
-        else:
-            _tasks = {}
+    def start():
+        """
+        全内存队列设计，操作全局变量的同时做到中断重载
+        优化在于深度遍历
+        """
+        # 如果没有消息，就尝试继承上次的消息队列
+        global _MsgTask, task_lock
+        try:
+            task_lock.acquire()
+            if len(_MsgTask) == 0:
+                if Course := _redis.get("_Telecha_Task"):
+                    _MsgTask = json.loads(Course)
+                    # print("队列:初始化上次检查的数据")
+        except Exception as err:
+            raise err
+        finally:
+            task_lock.release()
 
     @staticmethod
-    def save_tasks():
-        r.set('tasks', str(_tasks))
+    def create_data(user_id: str, group_id: str):
+        """
+        创建一个新的用户档案，使用当前时间值作为键名。
+        :param user_id:
+        :param group_id:
+
+        :return: key ,data 返回dict仿api字段,只许str类型键
+        """
+        if not user_id:
+            raise KeyError("not found user_id")
+        if not group_id:
+            raise KeyError("not found group_id")
+        group_id = str(group_id)
+        user_id = str(user_id)
+        # 上版的时间戳导致了重复的几率，所以采用新的队列算法
+        _time = str(int(time.time()))
+        _uukey = uuid.uuid5(uuid.NAMESPACE_DNS, f"{user_id}{group_id}")
+        return f"Task_{user_id}_{group_id}", {"user": user_id,
+                                              "group": group_id,
+                                              "time": _time,
+                                              "uuid": str(_uukey),
+                                              "interval": 200,
+                                              }
 
     @staticmethod
-    def readUser(where, group):
-        where = str(where)
-        JsonRedis.load_tasks()
-        if _tasks.get(where):
-            oss = _tasks[where].get(str(group))
-            if oss:
-                return oss
+    def read_user(userId: int):
+        """
+        从队列取出一个数据！
+        :param userId: 用户 ID
+        :return: 返回 groupId，和 key
+        """
+        global _MsgTask, task_lock
+        try:
+            task_lock.acquire()
+            groupKey = False
+            key = False
+            for item in _MsgTask:
+                _Data = _MsgTask[item]
+                if str(_Data.get("user")) == str(userId):
+                    groupKey = _Data.get("group")
+                    key = _Data.get("uuid")
+            return groupKey, key
+        except Exception as err:
+            raise err
+        finally:
+            task_lock.release()
+
+    @staticmethod
+    def resign_user(userId: int, groupId: int):
+        global _MsgTask, task_lock
+        try:
+            task_lock.acquire()
+            request_ = {"user_id": userId, "group_id": groupId}
+            _key, _profile = JsonRedis.create_data(**request_)
+            if not _MsgTask.get(_key):
+                _MsgTask[_key] = _profile
+                return _profile.get("uuid")
             else:
-                return []
+                return _MsgTask.get(_key).get("uuid")
+        except Exception as err:
+            raise err
+        finally:
+            task_lock.release()
+
+    @staticmethod
+    async def grant_resign(userId: int, groupId: int):
+        request_ = {"user_id": userId, "group_id": groupId}
+        _key, _data = JsonRedis.create_data(**request_)
+        if Data := _MsgTask.get(_key):
+            await JsonRedis.checker(unban=[_data])
+            return Data.get("uuid")
         else:
-            return []
+            return "没有记录"
 
     @staticmethod
-    def popUser(where, group, key):
-        where = str(where)
-        JsonRedis.load_tasks()
-        if _tasks.get(where):
-            if _tasks[where][str(group)]:
-                if key in _tasks[where][str(group)]:
-                    _tasks[where][str(group)].pop(key)
-        JsonRedis.save_tasks()
+    async def remove_user(userId: int, groupId: int):
+        request_ = {"user_id": userId, "group_id": groupId}
+        _key, _data = JsonRedis.create_data(**request_)
+        await JsonRedis.checker(dismiss=[_data])
 
     @staticmethod
-    def saveUser(where, group, key):
-        where = str(where)
-        JsonRedis.load_tasks()
-        if _tasks.get(where):
-            if _tasks[where].get(str(group)):
-                if not (key in _tasks[where][str(group)]):
-                    _tasks[where][str(group)].append(key)
-            else:
-                _tasks[where][str(group)] = []
-                _tasks[where][str(group)].append(key)
-        else:
-            _tasks[where] = {}
-            _tasks[where][str(group)] = []
-            _tasks[where][str(group)].append(key)
-        JsonRedis.save_tasks()
-
-    @staticmethod
-    def resign_user(userId, groupId):
-        """
-        注册队列
-        :param userId:
-        :param groupId:
-        :return:
-        """
-        JsonRedis.load_tasks()
-        key = str(int(time.time()))
-        _tasks["Time_id"][key] = str(userId)
-        _tasks["Time_group"][key] = str(groupId)
-        JsonRedis.save_tasks()
-        JsonRedis.saveUser("User_group", str(userId), key)
-        return key
-
-    @staticmethod
-    def read_user(userId):
-        """
-        读取用户的注册键
-        :param userId:
-        :return:
-        """
-        User = _tasks["User_group"].get(str(userId))
-        if User:
-            if len(User) != 0:
-                key = str(_tasks["User_group"].get(str(userId))[0])
-                # user = _tasks["Time_id"].get(key)
-                group = _tasks["Time_group"].get(key)
-                return group, key
-            else:
-                return False, False
-        else:
-            return False, False
-
-    @staticmethod
-    async def remove_user(userId, groupId):
-        """
-        人员被移除或者退群，弹出对目标群组的验证任务
-        :param userId:
-        :param groupId:
-        :return:
-        """
-        User = _tasks["User_group"].get(str(userId))
-        if User:
-            if len(User) != 0:
-                for key, i in _tasks["Time_group"].items():
-                    if i == str(groupId):
-                        await JsonRedis.checker(dismiss=[key])
-
-    @staticmethod
-    async def grant_resign(userId, groupId):
-        """
-        提升用户并取消过期队列,解禁需要另外语句
-        :param userId:
-        :param groupId:
-        :return:
-        """
-        User = _tasks["User_group"].get(str(userId))
-        if User:
-            if len(User) != 0:
-                for key, i in _tasks["Time_group"].items():
-                    if i == str(groupId):
-                        await JsonRedis.checker(tar=[key])
-                        return key
-                    else:
-                        return "没有相关记录"
-            return "错误:你没有在等待队列中"
-        # JsonRedis().remove_user(userId, groupId)
-        # JsonRedis.saveUser("super", str(userId), str(groupId))
-        # else:
-        #     key = _tasks["User_group"].get(str(userId))[0]
-        #     # groupId = _tasks["Time_group"].get(key)
-        #     # JsonRedis.saveUser("super", str(userId), str(groupId))
-        #     await JsonRedis.checker(tar=[key])
-
-    @staticmethod
-    async def checker(tar=None, fail_user=None, dismiss=None):
-        """
-        检查器，调用就会检查一次，弹出传入的键值，
-        :param dismiss: 传入这里，弹出且不做操作
-        :param tar: 传入这里，则不踢出而弹出
-        :param fail_user: 传入这里，则踢出且弹出
-        :return:
-        """
-        group = False
-        user = False
+    async def checker(ban=None, unban=None, dismiss=None):
+        global _MsgTask, task_lock
         if dismiss is None:
             dismiss = []
-        if tar is None:
-            tar = []
-        if fail_user is None:
-            fail_user = []
-        dealList = [] + fail_user + tar + dismiss
-        # 插队key和检查过期Key
-        JsonRedis.load_tasks()
-        for key, item in _tasks["Time_id"].items():
-            if abs(int(time.time()) - int(key)) > int(_tasks["interval"]):
-                dealList.append(key)
-
-        for key in dealList:
-            try:
-                user = _tasks["Time_id"].pop(str(key))
-                group = _tasks["Time_group"].pop(str(key))
-                _tasks["User_group"].get(str(user)).remove(str(key))
-            except Exception as e:
-                print(e)
-            if key not in dismiss:
+        if unban is None:
+            unban = []
+        if ban is None:
+            ban = []
+        # 筛选数据
+        for key in _MsgTask:
+            _data = _MsgTask[key]
+            if abs(int(time.time()) - int(_data["time"])) > int(_data["interval"]):
+                ban.append({"user": _data["user"], "group": _data["group"]})
+        # 先处理掉 dismiss 的数据，然后对数据进行检查，添加到执行队列封杀
+        try:
+            for key in unban:
+                profile = key
+                userId = profile.get("user")
+                groupId = profile.get("group")
                 from Bot.Controller import clientBot
                 bot, config = clientBot().botCreate()
-                if key in tar:
-                    await bot.approve_chat_join_request(chat_id=group, user_id=user)
-                else:
-                    try:
-                        if group and user:
-                            await bot.delete_state(user, group)
-                            await bot.decline_chat_join_request(chat_id=group, user_id=user)
-                            await bot.ban_chat_member(chat_id=group, user_id=user,
-                                                      until_date=datetime.datetime.timestamp(
-                                                          datetime.datetime.now() + datetime.timedelta(minutes=12)))
-                            await bot.send_message(user, f"验证失败，此群组会话验证需要冷却 12 分钟")
-                    except Exception as e:
-                        print(e)
-                    # print("ban " + str(user) + str(group))
-        JsonRedis.save_tasks()
+                await bot.delete_state(userId, groupId)
+                await bot.approve_chat_join_request(chat_id=groupId, user_id=userId)
+            for key in ban:
+                profile = key
+                userId = profile.get("user")
+                groupId = profile.get("group")
+                # print(f"释放{uuid}，{userId}-{groupId}")
+                if groupId and userId:
+                    from Bot.Controller import clientBot
+                    bot, config = clientBot().botCreate()
+                    await bot.delete_state(userId, groupId)
+                    await bot.decline_chat_join_request(chat_id=groupId, user_id=userId)
+                    await bot.ban_chat_member(chat_id=groupId, user_id=userId,
+                                              until_date=datetime.datetime.timestamp(
+                                                  datetime.datetime.now() + datetime.timedelta(minutes=12)))
+                    await bot.send_message(userId, f"验证失败或超时，此群组会话验证需要冷却 12 分钟")
+        except Exception as err:
+            print(err)
+
+        # 释放数据
+        try:
+            task_lock.acquire()
+            allL = []
+            allL.extend(ban)
+            allL.extend(dismiss)
+            allL.extend(unban)
+            # print(allL)
+            for key in allL:
+                profile = key
+                userId = profile.get("user")
+                groupId = profile.get("group")
+                _key = JsonRedis.crateKey(user_id=userId, group_id=groupId)
+                _MsgTask.pop(_key, None)
+        except Exception as err:
+            raise err
+        finally:
+            task_lock.release()
+            _redis.set("_Telecha_Task", json.dumps(_MsgTask))
 
     @staticmethod
-    async def run_timer():
-        await JsonRedis.checker()
-        JsonRedis.timer()
-
-    @staticmethod
-    def timer():
-        from threading import Timer
-        t = Timer(3, JsonRedis.run_timer)
-        t.start()
-
-#########################################
-# JsonRedis(6).add(1222, str(-52333))
-#
-# JsonRedis.timer()
-#
-# print(2)
+    def crateKey(user_id, group_id):
+        return f"Task_{user_id}_{group_id}"
