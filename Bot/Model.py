953795dbba90acdf5a8cf96ede31dc14bc891cd9
sudoskys <coldlando@hotmail.com>
2022-10-07 15:31:25 +0800

    feat(project):取消通过/不通过通知

--- Bot/Model.py
+++ Bot/Model.py
@@ -477,7 +477,7 @@ async def Verify2(bot, message, config):
                 await bot.reply_to(message, '回答错误!')
                 # 不通知群组
                 # msgs = await botWorker.send_ban(message, bot, group_k)
-                aioschedule.every(360*2).seconds.do(botWorker.unbanUser, bot, group_k, message.from_user.id).tag(
+                aioschedule.every(360 * 2).seconds.do(botWorker.unbanUser, bot, group_k, message.from_user.id).tag(
                     message.from_user.id * abs(group_k))
                 # aioschedule.every(25).seconds.do(botWorker.delmsg, msgs.chat.id, msgs.message_id).tag(
                 #    msgs.message_id * abs(msgs.chat.id))
